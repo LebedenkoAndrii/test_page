07df727876540390cf614cff1a3b7b9e14ca4a0a
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-07-17 18:22:57 +0300

    made other section, added adaptive for mobile

---
+++ js/nav.js
@@ -0,0 +1,6 @@
+let navBtn = document.querySelector(".mob_nav_btn");
+let mobNav = document.querySelector(".mob_nav");
+
+navBtn.addEventListener("click", function () {
+  mobNav.classList.toggle("active");
+});
